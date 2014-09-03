541a442e74d0cc8f2efacd64d690e653c8e56468
Markus Beuckelmann <email@markus-beuckelmann.de>
2014-09-03 15:40:47 +0200

    Reorganize package structure

---
+++ lltk-restful/config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+config = {}
+
+config['name'] = 'LLTK-RESTful'
+config['host'] = '127.0.0.1'
+config['port'] = 5001
+
+config['debug'] = False
+config['caching'] = True
